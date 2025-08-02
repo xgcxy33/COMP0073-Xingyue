8f0c04aab4569195a863b1ec7efe63f5c900fa35
xingyue.chen <chenxingyue783@gmail.com>
2025-08-02 23:34:56 +0100

    allow text_generation_pipeline.py write result to files

--- text_generation_pipeline.py
+++ text_generation_pipeline.py
@@ -1,9 +1,11 @@
 from deepseek_janus import init_deepseek_janus, generate_multimodal_understanding
 from tgi_utils import get_tgi_stream
+from datetime import datetime
 
 import argparse
 import json
 import prompts
+import os
 
 
 def generate_combined_model_response(image, args, vl_chat_processor, vl_gpt, tokenizer):
@@ -49,6 +51,7 @@ if __name__ == "__main__":
     parser.add_argument("--clinic_model_prompt_key", default="default_clinic_prompt")
     parser.add_argument("--clinic_model_id", default="aaditya/Llama3-OpenBioLLM-70B")
     parser.add_argument("--clinic_model_tgi_port", default=8090)
+    parser.add_argument("--output_file_path", default=None)
 
     # Parse the arguments
     args = parser.parse_args()
@@ -58,12 +61,32 @@ if __name__ == "__main__":
     input_file_data = None
     with open(args.input_file_path, 'r') as input_file:
         input_file_data = json.load(input_file)
+
+    res = vars(args)
+    res["outputs"] = []
     for record in input_file_data:
-        res = generate_combined_model_response(image=record["image_path"], 
+        generated_text = generate_combined_model_response(image=record["image_path"], 
                                                args=args,
                                                vl_chat_processor=vl_chat_processor, 
                                                vl_gpt=vl_gpt, 
                                                tokenizer=tokenizer)
-        print(res)
+        record["clinic_output"] = generated_text
+        res["outputs"].append(record)
+
+    output_file_path = args.output_file_path
+    if not output_file_path:
+        input_filename = os.path.basename(args.input_file_path)
+        input_filename = input_filename.split('.', maxsplit=1)[0]
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+        output_file_path = f"./output/{input_filename}_{timestamp}.json"
+
+    # Ensure parent directory exists
+    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+
+    with open(output_file_path, "w") as output_file:
+        json.dump(res, output_file)
+        
+        
         
     
